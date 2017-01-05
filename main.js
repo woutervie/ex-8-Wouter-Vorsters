57f84d31f3ea72d1cb1c84210c4a4f21833aa974
wouter vorsters <r0258931@student.thomasmore.be>
2017-01-05 17:34:11 +0100

    Laatste aanpassing vergeten te committen, aanpassing is wel ingediend via Toledo

--- main.js
+++ main.js
@@ -8,18 +8,18 @@ process.env.NODE_TLS_REJECT_UNAUTHORIZED = "0";
 
 var BASE_URL = "https://web-ims.thomasmore.be/datadistribution/API/2.0";
 var Settings = function (url) {
-	this.url = BASE_URL + url;
-	this.method = "GET";
-	this.qs = {format: 'json'};
-	this.headers = {
-		authorization: "Basic aW1zOno1MTJtVDRKeVgwUExXZw=="
-	};
+    this.url = BASE_URL + url;
+    this.method = "GET";
+    this.qs = {format: 'json'};
+    this.headers = {
+            authorization: "Basic aW1zOno1MTJtVDRKeVgwUExXZw=="
+    };
 };
 
 var Drone = function (id, name, mac) {
-	this._id = id;
-	this.name = name;
-	this.mac = mac;
+    this._id = id;
+    this.name = name;
+    this.mac = mac;
 };
 
 var File = function (id, drone_ref) {
@@ -28,13 +28,14 @@ var File = function (id, drone_ref) {
     this.drone_ref = drone_ref;
 };
 
-var Content = function (id, mac, datetime, rssi, file_ref) {
+var Content = function (id, mac, datetime, rssi, file_ref, drone_ref) {
     this._id = id;
     this.id = id;
     this.mac = mac;
     this.datetime = datetime;
     this.rssi = rssi;
     this.file_ref = file_ref;
+    this.drone_ref = drone_ref;
 };
 
 var dronesSettings = new Settings("/drones?format=json");
@@ -45,8 +46,6 @@ dal.clearContent();
 
 request(dronesSettings, function (error, response, dronesString) {
 	var drones = JSON.parse(dronesString);
-	//console.log(drones);
-	//console.log("***************************************************************************");
 	drones.forEach(function (drone) {
 		var droneSettings = new Settings("/drones/" + drone.id + "?format=json");
 		request(droneSettings, function (error, response, droneString) {
@@ -60,11 +59,9 @@ request(dronesSettings, function (error, response, dronesString) {
                                 dal.insertFile(new File(file.id, file.ref));
                                 
                                 var contentSettings = new Settings("/files/" + file.id + "/contents?format=json");
-                                //console.log(contentSettings.url);
                                 request(contentSettings, function (error, response, contentsString) { 
                                     if (!error && response.statusCode === 200) {
                                         var contents = JSON.parse(contentsString);
-                                        //console.log(contents);
                                                                                                  
                                         contents.forEach(function(content) {
                                             var contentSettings = new Settings("/" + content.ref + "/contents/" + content.id + "?format=json");
@@ -72,7 +69,7 @@ request(dronesSettings, function (error, response, dronesString) {
                                             request(contentSettings, function (error, response, contentString) {
                                                 if (!error && response.statusCode === 200) {    
                                                     var content = JSON.parse(contentString);
-                                                    dal.insertContent(new Content(content.id, content.mac_address, content.datetime, content.rssi, content.ref));
+                                                    dal.insertContent(new Content(content.id, content.mac_address, content.datetime, content.rssi, content.ref, drone.name));
                                                 }                                         
                                             });
                                         }); 
@@ -83,5 +80,3 @@ request(dronesSettings, function (error, response, dronesString) {
 		});
 	});
 });
-
-console.log("Hello World!");
